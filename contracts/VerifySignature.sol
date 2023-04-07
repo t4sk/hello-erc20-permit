24d886e749e69e300dcce43aa11db2d6ede7ba40
new4er <>
2023-04-07 10:49:09 +0800

    verify chain id

--- contracts/VerifySignature.sol
+++ contracts/VerifySignature.sol
@@ -6,19 +6,21 @@ contract VerifySignature {
         address _to,
         uint _amount,
         string memory _message,
-        uint _nonce
+        uint _nonce,
+        uint _chainid
     ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_to, _amount, _message, _nonce));
+        return keccak256(abi.encodePacked(_to, _amount, _message, _nonce, _chainid));
     }
 
-    function getEthSignedMessageHash(bytes32 _messageHash)
-        public
-        pure
-        returns (bytes32)
-    {
+    function getEthSignedMessageHash(
+        bytes32 _messageHash
+    ) public pure returns (bytes32) {
         return
             keccak256(
-                abi.encodePacked("\x19Ethereum Signed Message:\n32", _messageHash)
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    _messageHash
+                )
             );
     }
 
@@ -28,33 +30,28 @@ contract VerifySignature {
         uint _amount,
         string memory _message,
         uint _nonce,
+        uint _chainid,
         bytes memory signature
-    ) public pure returns (bool) {
-        bytes32 messageHash = getMessageHash(_to, _amount, _message, _nonce);
+    ) public view returns (bool) {
+        require(_chainid == block.chainid, "ChainId Incorrect");
+        bytes32 messageHash = getMessageHash(_to, _amount, _message, _nonce, _chainid);
         bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
 
         return recoverSigner(ethSignedMessageHash, signature) == _signer;
     }
 
-    function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature)
-        public
-        pure
-        returns (address)
-    {
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
         (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
 
         return ecrecover(_ethSignedMessageHash, v, r, s);
     }
 
-    function splitSignature(bytes memory sig)
-        public
-        pure
-        returns (
-            bytes32 r,
-            bytes32 s,
-            uint8 v
-        )
-    {
+    function splitSignature(
+        bytes memory sig
+    ) public pure returns (bytes32 r, bytes32 s, uint8 v) {
         require(sig.length == 65, "invalid signature length");
 
         assembly {
